9d4655b22c847a18105e98446a8f886fb0889d53
Lex Toumbourou <lex.toumbourou@team.telstra.com>
2012-10-19 15:58:37 +1100

    Moved Nagios component into plugins directory

--- GiantDwarf.py
+++ GiantDwarf.py
@@ -1,99 +1,71 @@
-from sys import exit
 from time import sleep
 from datetime import datetime
+import sys
+import pkgutil
+import os
 import logging
 
 from pyfire import Campfire
 from BeautifulSoup import BeautifulSoup
 
-from lib import utils
 import settings
 
+class GiantDwarf():
+    def __init__(self):
+        self.plugins = []
+        self.is_connected = False
+        self.room = None;
 
-def get_nagios_events(html):
-    """
-    Return latest Nagios event (based on last polling period)
-    as an array of dictionaries
-    """
-    output = []
-    soup = BeautifulSoup(html, convertEntities=BeautifulSoup.HTML_ENTITIES)
-    try:
-        trs = soup.find('table', 'notifications').findAll('tr')
-    except AttributeError:
-        # We didn't find anything, don't bother parsing it
+        self.load_all_plugins()
+
+    def _get_class_name(self, mod_name):
+        """Return the class name from a plugin name"""
+        output = ""
+
+        # Split the _ and ignore the 1st word plugin
+        words = mod_name.split('_')[1:]
+
+        # Capitalise the first letter of each word and add to string
+        for word in words:
+            output += word.title()
+
         return output
 
-    if not trs:
-        return output
+    def load_all_plugins(self):
+        path = os.path.join(os.path.dirname(__file__), 'plugins')
+        modules = pkgutil.iter_modules(path=[path])
 
-    for tr in trs[2:]:
-        td = tr.findAll('td')
-        host = td[0].a.text
-        try:
-            service = td[1].a.text
-        except AttributeError:
-            service = "N/A"
-        level = td[2].text
-        time = td[3].text
-        info = td[6].text
-        contact = td[4].text
-        # Ignore pager notifications
-        if contact.endswith('pager'):
-            continue
-        output.append({'host': host,
-                       'service': service,
-                       'level': level,
-                       'time': utils.to_datetime(time),
-                       'info': info})
-    return output
+        for loader, mod_name, ispkg in modules:
+            # Ensure that module isn't already loaded
+            if mod_name not in sys.modules:
+                # import module
+                loaded_mod = __import__(path+"."+mod_name, fromlist=[mod_name])
 
+                # load class from imported module
+                class_name = self._get_class_name(mod_name)
+                loaded_class = getattr(loaded_mod, class_name)
 
-def send_to_room(last_run, room):
+                # create an instance of the class
+                self.plugins.append(loaded_class())
+
+    def start_campfire(self):
+        """
+        Return a room object
+        """
+        # Setup Campfire and join our room
+        c = Campfire(settings.SUBDOMAIN,
+                     settings.TOKEN,
+                     'x',
+                     ssl=settings.USE_SSL)
+        self.is_connected = True
+        self.room = c.get_room_by_name(settings.ROOM)
+
+class GiantDwarfPlugin():
     """
-    Sends the event information to the Campfire room
-    and return the last event's time
+    Each plugin should be a subclass of this
     """
-    html = utils.open_page()
-    if not html:
-        return last_run
-
-    nagios_events = get_nagios_events(html)
-    if not nagios_events:
-        return last_run
-
-    for event in nagios_events:
-        if event['time'] <= last_run:
-            # Event is older then last event, skip
-            continue
-
-        # Use Alien man if no icon has been defined for the event level
-        try:
-            icon = settings.ALERT_ICONS[event['level']]
-        except KeyError:
-            icon = ':alien:'
-
-        msg = "{0} {1} | {2} | {3} | {4}".format(icon,
-                                                 event['time'],
-                                                 event['service'],
-                                                 event['host'],
-                                                 event['info'])
-        # Say what the hell is going down!
-        room.speak(msg)
-
-    # We have a new last run time
-    return nagios_events[-1]['time']
-
-
-def start_campfire():
-    """
-    Return a room object
-    """
-    # Setup Campfire and join our room
-    c = Campfire(settings.SUBDOMAIN,
-                 settings.TOKEN,
-                 'x',
-                 ssl=settings.USE_SSL)
-    return c.get_room_by_name(settings.ROOM)
+    def run(self):
+        pass
 
 if __name__ == '__main__':
     # Configure logging
@@ -103,24 +75,25 @@ if __name__ == '__main__':
     # Ensure we don't get old Nagios events
     last_run = datetime.now()
     logging.info("Online and ready")
-    is_connected = False
+    gd = GiantDwarf()
 
     while True:
-        if not is_connected:
-            room = start_campfire()
-            room.join()
+        if not gd.is_connected:
+            gd.start_campfire()
+            gd.room.join()
         try:
-            last_run = send_to_room(last_run, room)
+            last_run = datetime.now()
+            for plugin in gd.plugins:
+                plugin.run(gd)
             logging.info("Last message @ {0}".format(last_run))
-            is_connected = True
         except KeyboardInterrupt:
             logging.info("Leaving the room")
-            room.leave()
+            gd.room.leave()
             exit()
         except Exception, e:
             # I don't want GiantDwarf dying over an exception
             # this allows it to pass and try again next period
             logging.warning("Exception occured: ", e)
-            is_connected = False
+            gd.is_connected = False
 
         sleep(settings.FETCH_INTERVAL)
