354c88ff12722bd2d6e7d93e8e99a69a7ebca8fa
Lex Toumbourou <lex.toumbourou@team.telstra.com>
2013-01-23 13:15:12 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='GiantDwarf',
-      version='0.5',
+      version='0.6',
       description='A simple Campfire bot written in Python',
       author='Lex Toumbourou',
       author_email='lextoumbourou@gmail.com',
